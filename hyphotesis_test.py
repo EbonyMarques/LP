ec9f26acd637ebd67d4e4663aa154d3793d6e266
EbonyMarques <ebony.marquesr@ufrpe.br>
2019-09-19 13:04:52 -0300

    Update hyphotesis_test.py

--- hyphotesis_test.py
+++ hyphotesis_test.py
@@ -1,30 +1,32 @@
 import csv
 
-def reciever(row):
+def appender(row, word, n):
     global reviews
     
     if len(reviews) < n:
-        if "nice" in row[6]:
+        if word in row[6]:
             reviews.append(row)
 
-def other_reciever(text):
+def counter(recommendation):
     global count
     
-    if text == "Not Recommended":
+    if recommendation == "Not Recommended":
         count += 1
 
-reviews = []
-count = 0
-
+#hypothesis#
 n = 1365
 p = 0.15
 
 h0 = "p = " + str(n*p)
 h1 = "p != " + str(n*p)
+############
+
+reviews = []
+word = "good"
+count = 0
 
 print("n = %i" %(n))
 print("p = %.2f\n" %(p))
-
 print("H0: %s" %(h0))
 print("H1: %s\n" %(h1))
 
@@ -33,11 +35,11 @@ with open("steam_reviews.csv", encoding = "utf-8") as file:
     reader = list(reader)
     reader.pop(0)
 
-    list(map(lambda row: reciever(row[0].split(",")), reader))
+    list(map(lambda row: appender(row[0].split(","), word, n), reader))
 
-list(map(lambda review: other_reciever(review[5]), reviews))
+list(map(lambda review: counter(review[5]), reviews))
 
-print("%i/%i bad reviews with 'nice'!\n" %(count, n))
+print("%i/%i bad reviews with '%s'!\n" %(count, n, word))
 
 if count == n*p:
     print("H0 is accepted.\n%s" %(h0))

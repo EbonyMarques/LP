7ffccab5f7a58862a58bda79f8bf5a3a53ed1a65
EbonyMarques <ebony.marquesr@ufrpe.br>
2019-09-24 21:58:53 -0300

    Update hyphotesis_test.py

--- hyphotesis_test.py
+++ hyphotesis_test.py
@@ -1,11 +1,21 @@
+from nltk import FreqDist, word_tokenize
+from statsmodels.stats import weightstats as stests
+import rpy2
 import csv
 
-def appender(row, word, n):
-    global reviews
+def appender_nices(row, word, n):
+    global nices
     
-    if len(reviews) < n:
-        if word in row[6]:
-            reviews.append(row)
+    if len(nices) < n:
+        if word in word_tokenize(row[6]) and row[5] == "Recommended":
+            nices.append(row)
+
+def appender_goods(row, word, n):
+    global goods
+    
+    if len(goods) < n:
+        if word in word_tokenize(row[6]) and row[5] == "Recommended":
+            goods.append(row)
 
 def counter(recommendation):
     global count
@@ -13,36 +23,46 @@ def counter(recommendation):
     if recommendation == "Not Recommended":
         count += 1
 
-#hypothesis#
-n = 1365
-p = 0.15
+def word_counts(text, words):
+    token = word_tokenize(text)
+    counts = FreqDist(token)
 
-h0 = "p = " + str(n*p)
-h1 = "p != " + str(n*p)
-############
+    result = list(map(lambda x: counts[x] or 0, words))
 
-reviews = []
-word = "nice"
-count = 0
+    return result[0]
 
-print("n = %i" %(n))
-print("p = %.2f\n" %(p))
-print("H0: %s" %(h0))
-print("H1: %s\n" %(h1))
+nices, goods, nices_counts, goods_counts = [], [], [], []
+counts = 0
 
 with open("steam_reviews.csv", encoding = "utf-8") as file:
     reader = csv.reader(file, delimiter = ";")
     reader = list(reader)
     reader.pop(0)
 
-    list(map(lambda row: appender(row[0].split(","), word, n), reader))
+    list(map(lambda row: appender_nices(row[0].split(","), "nice", 1365), reader))
+    list(map(lambda row: appender_goods(row[0].split(","), "good", 1365), reader))
 
-list(map(lambda review: counter(review[5]), reviews))
+for review in nices:
+    nices_counts.append(word_counts(review[6], ["nice"]))
 
-print("%i/%i bad reviews with '%s'!\n" %(count, n, word))
+for review in goods:
+    goods_counts.append(word_counts(review[6], ["good"]))
 
-if count == n*p:
-    print("H0 is not rejected.\n%s" %(h0))
+ztest, pval = stests.ztest(nices_counts, goods_counts)
+
+a, b = 0, 0
+
+for i in nices_counts:
+    a += i
+
+for i in goods_counts:
+    b += i
+
+print("H0: a/1365 = b/1365")
+print("H1: a/1365 != b/1365")
+
+if pval == 0.05:
+    print("H0 is not rejected")
 
 else:
-    print("H0 is rejected.\n%i != %.2f" %(count, n*p))
+    print("H0 is rejected")

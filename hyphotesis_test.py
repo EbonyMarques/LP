284f2c7cf39105f3a441e140216e66dd781b405e
EbonyMarques <ebony.marquesr@ufrpe.br>
2019-10-08 21:56:45 -0300

    Update hyphotesis_test.py

--- hyphotesis_test.py
+++ hyphotesis_test.py
@@ -1,67 +1,56 @@
 from nltk import FreqDist, word_tokenize
 from statsmodels.stats import weightstats
-from numpy import average
 import csv
 
-reviews, first_word_counts, second_word_counts = [], [], []
-first_word, second_word, n, recommendation = "nice", "good", 4350, "Recommended"
+first_word_counts, second_word_counts = [], []
+first_word, second_word = "nice", "good"
+quantity, recommendationType = 1365, "Recommended"
 
-def appender(row, recommendation, amount):
-    global reviews
-
-    if len(reviews) < amount:
-        if recommendation == "":
-            reviews.append(row)
-
-        elif recommendation == row[5]:
-            reviews.append(row)
-
-def first_word_counter(text, word):
+def first_word_appender(row):
     global first_word_counts
-
-    first_word_counts.append(word_counts(text, [word]))
+    
+    if len(first_word_counts) < quantity:
+        if row[5] == recommendationType and first_word in word_tokenize(row[6]):
+            first_word_counts.append(word_counts(row[6], [first_word]))
             
-def second_word_counter(text, word):
+def second_word_appender(row):
     global second_word_counts
-
-    second_word_counts.append(word_counts(text, [word]))
+    
+    if len(second_word_counts) < quantity:
+        if row[5] == recommendationType and second_word in word_tokenize(row[6]):
+            second_word_counts.append(word_counts(row[6], [second_word]))
 
 def word_counts(text, words):
-    token = word_tokenize(text)
-    counts = FreqDist(token)
+    counts = FreqDist(word_tokenize(text))
 
-    result = list(map(lambda x: counts[x] or 0, words))
-
-    return result[0]
+    return list(map(lambda x: counts[x] or 0, words))[0]
 
 with open("steam_reviews.csv", encoding = "utf-8") as file:
     reader = csv.reader(file, delimiter = ";")
     reader = list(reader)
     reader.pop(0)
 
-    list(map(lambda row: appender(row[0].split(","), recommendation, n), reader))
-
-    for i in reviews:
-        first_word_counter(i[6], first_word)
-        second_word_counter(i[6], second_word)
+    list(map(lambda row: first_word_appender(row[0].split(",")), reader))
+    list(map(lambda row: second_word_appender(row[0].split(",")), reader))
 
 ztest, pval = weightstats.ztest(first_word_counts, second_word_counts)
+"""
+a, b = 0, 0
 
-a, b = average(first_word_counts), average(second_word_counts)
+for i in first_word_counts:
+    a += i
 
-print("a = sum of all occurrences of %s in %s reviews" %(first_word, n))
-print("b = sum of all occurrences of %s in %s reviews\n" %(second_word, n))
+for i in second_word_counts:
+    b += i
 
-print("Recommendation type: %s" %(recommendation))
+print("H0: a/4350 = b/4350")
+print("H1: a/4350 != b/4350\n")
 
-print("\nH0: a/%s != b/%s"%(n, n))
-print("H1: a/%s = b/%s\n"%(n, n))
-
-print("a/%s = %f" %(n, a))
-print("b/%s = %f" %(n, b))
-
+print("a/4350 = " + str(a/1365))
+print("b/4350 = " + str(b/1365) + "\n")
+"""
 if pval < 0.05:
-    print("\nH0 is not rejected")
+    print("H0 is not rejected")
 
 else:
-    print("\nH0 is rejected")
+    print("H0 is rejected")

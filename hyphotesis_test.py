3bf10c86a43b7798e6de9d5885a06ff6bcec91ef
EbonyMarques <ebony.marquesr@ufrpe.br>
2019-10-09 21:34:19 -0300

    Update hyphotesis_test.py

--- hyphotesis_test.py
+++ hyphotesis_test.py
@@ -1,9 +1,10 @@
 from nltk import FreqDist, word_tokenize
 from statsmodels.stats import weightstats
+from numpy import average
 import csv
 
 first_word_counts, second_word_counts = [], []
-first_word, second_word = "nice", "good"
+first_word, second_word = "excellent", "trash"
 quantity, recommendationType = 1365, "Recommended"
 
 def first_word_appender(row):
@@ -35,8 +36,21 @@ with open("steam_reviews.csv", encoding = "utf-8") as file:
 
 ztest, pval = weightstats.ztest(first_word_counts, second_word_counts)
 
+first_average, second_average = average(first_word_counts), average(second_word_counts)
+
+print("first = sum of all occurrences of %s in %s reviews" %(first_word, quantity))
+print("second = sum of all occurrences of %s in %s reviews\n" %(second_word, quantity))
+
+print("Type: %s" %(recommendationType))
+
+print("\nH0: first/%s = second/%s"%(quantity, quantity))
+print("H1: first/%s != second/%s\n"%(quantity, quantity))
+
+print("first/%s = %f" %(quantity, first_average))
+print("second/%s = %f" %(quantity, second_average))
+
 if pval < 0.05:
-    print("H0 is not rejected")
+    print("\nH0 is not rejected")
 
 else:
-    print("H0 is rejected")
+    print("\nH0 is rejected")

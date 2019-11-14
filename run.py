94686565ca1b562ca00ab4a7d1c147b6eb39f981
EbonyMarques <ebony.marquesr@ufrpe.br>
2019-11-14 08:51:15 -0300

    Update run.py

--- run.py
+++ run.py
@@ -4,22 +4,14 @@ from count_vectorizer import CountVectorizer
 from tfidf_vectorizer import TFIDFVectorizer
 from naive_bayes_classifier import NaiveBayes
 from svm_classifier import SVM
-from plot_graphic import plot_graphic
+#from plot_graphic import plot_graphic
 #from word_count import cloud
 
 extractor = Extractor()
 reviews = extractor.reviews
 recommendations = extractor.recommendations
-#freq_dist = tokenizer(reviews)
-nameN, valuesN, resultN = reviews_negative(reviews)
-namePP, valuesPP, namePN, valuesPN = words_separation(reviews)
-plot_graphic("Negative words", namePN, valuesPN)
-plot_graphic("Positive words", namePP, valuesPP)
-plot_graphic("Negative reviews' words", nameN, valuesN)
 
-#cloud(extractor.freq_dist)
-
-'''vectorizer1 = CountVectorizer(reviews, recommendations)
+vectorizer1 = CountVectorizer(reviews, recommendations)
 vectorizer2 = CountVectorizer(reviews, recommendations, (1, 4))
 vectorizer3 = TFIDFVectorizer(reviews, recommendations)
 vectorizer4 = TFIDFVectorizer(reviews, recommendations, (1, 4))
@@ -29,8 +21,6 @@ classifier2 = NaiveBayes(vectorizer2, "naive_bayes_classifier_5000_1_4.pickle")
 classifier3 = SVM(vectorizer3,"svm_classifier_5000_1_1.pickle")
 classifier4 = SVM(vectorizer4, "svm_classifier_5000_1_4.pickle")
 
-plot_graphic("F-measure", ["SVM(1,1)", "Naive(1,1)", "SVM(1,4)", "Naive(1,4)"], [classifier3.getFmeasure(),classifier1.getFmeasure(), classifier4.getFmeasure(), classifier2.getFmeasure()])
-
 print("Naive 1-1")
 classifier1.accuracy_printer()
 print("Naive 1-4")
@@ -39,7 +29,17 @@ print("SVM 1-1")
 classifier3.accuracy_printer()
 print("SVM 1-4")
 classifier4.accuracy_printer()
-'''
+
+#freq_dist = tokenizer(reviews)
+#nameN, valuesN, resultN = reviews_negative(reviews)
+#namePP, valuesPP, namePN, valuesPN = words_separation(reviews)
+
+#plot_graphic("Negative words", namePN, valuesPN)
+#plot_graphic("Positive words", namePP, valuesPP)
+#plot_graphic("Negative reviews' words", nameN, valuesN)
+#plot_graphic("F-measure", ["SVM(1,1)", "Naive(1,1)", "SVM(1,4)", "Naive(1,4)"], [classifier3.getFmeasure(),classifier1.getFmeasure(), classifier4.getFmeasure(), classifier2.getFmeasure()])
+#cloud(extractor.freq_dist)
+
 """
 
 stop = False

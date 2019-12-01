5a910e535973d02103d87bf50e4cc4eb39316abf
EbonyMarques <ebony.marquesr@ufrpe.br>
2019-12-01 05:17:08 -0300

    Final updates.

--- run.py
+++ run.py
@@ -9,13 +9,12 @@ from time import *
 extractor = Extractor()
 reviews = extractor.reviews
 recommendations = extractor.recommendations
+times = []
 
 vectorizer1 = TFIDFVectorizer(reviews, recommendations)
 vectorizer2 = TFIDFVectorizer(reviews, recommendations, (1, 4))
 vectorizer3 = TFIDFVectorizer(reviews, recommendations)
 vectorizer4 = TFIDFVectorizer(reviews, recommendations, (1, 4))
-
-times = []
 
 time1 = [float(time())]
 classifier3 = SVM(vectorizer3, "data/svm_classifier_5000_1_1.pickle")
@@ -37,29 +36,23 @@ classifier2 = NaiveBayes(vectorizer2, "data/naive_bayes_classifier_5000_1_4.pick
 time4.append(float(time()))
 times.append(time4[1]-time4[0])
 
-classifiers_names = ["SVM (1,1)", "Naive (1,1)", "SVM (1,4)", "Naive (1,4)"]
-
-thread13 = Thread(target=plot_graphic, args=["Train times", classifiers_names, [10582.61030960083/60,0.030913352966308594/60,11155.967951536179/60,0.2752652168273926/60], "Algorithm (ngram_range)", "Minutes", "graphics/times.png"])
-thread13.start()
-thread13.join()
 
 """
+classifiers_names = ["SVM (1,1)", "Naive (1,1)", "SVM (1,4)", "Naive (1,4)"]
+
 result1 = classifier1.scorer()
 result2 = classifier2.scorer()
 result3 = classifier3.scorer()
 result4 = classifier4.scorer()
 
-
 accuracy = [result3[0], result1[0], result4[0], result2[0]]
 precision = [result3[1], result1[1], result4[1], result2[1]]
 fmeasure = [result3[2], result1[2], result4[2], result2[2]]
-"""
-"""
+
 namePP, valuesPP, namePN, valuesPN = words_separation(reviews)
 nameN, valuesN, resultN = reviews_function(reviews[5000:])
 nameP, valuesP, resultP = reviews_function(reviews[:4999])
 nameT, valuesT, resultT = reviews_function(reviews)
-"""
 
 #thread1 = Thread(target=plot_graphic, args=["Accuracy", classifiers_names, accuracy, "Algorithm (ngram_range)", "Percentage", "graphics/accuracy.png"])
 #thread2 = Thread(target=plot_graphic, args=["Precision", classifiers_names, precision, "Algorithm (ngram_range)", "Percentage", "graphics/precision.png"])
@@ -69,12 +62,13 @@ nameT, valuesT, resultT = reviews_function(reviews)
 #thread6 = Thread(target=plot_graphic, args=["Negative reviews' most frequent words", nameN, valuesN, "Word", "Occurrence", "graphics/frequent_words_negative_reviews.png"])
 #thread7 = Thread(target=plot_graphic, args=["Positive reviews' most frequent words", nameP, valuesP, "Word", "Occurrence", "graphics/frequent_words_positive_reviews.png"])
 #thread8 = Thread(target=plot_graphic, args=["Most frequent words in positive and negative reviews", nameT, valuesT, "Word", "Occurrence", "graphics/frequent_words_positive_and_negative_reviews.png"])
-"""
+
 thread9 = Thread(target=plot_graphic, args=["Most frequent positive words in positive reviews", ["fun", "play", "like", "friend", "great", "good", "love", "recommend", "best"], [2590, 2491, 1585, 1514, 1383, 1377, 754, 711, 685], "Word", "Occurrence", "graphics/frequent_positive_words_positive_reviews.png"])
 thread10 = Thread(target=plot_graphic, args=["Most frequent negative words in positive reviews", ["issue", "bug", "problem", "bad", "toxic", "hate", "negative", "disconnect", "difficult"], [749, 378, 361, 320, 240, 133, 116, 107, 107], "Word", "Occurrence", "graphics/frequent_negative_words_positive_reviews.png"])
 thread11 = Thread(target=plot_graphic, args=["Most frequent negative words in negative reviews", ["hacker", "banned", "community", "ban", "bad", "issue", "cheater", "problem", "bug"], [773, 744, 660, 513, 433, 401, 319, 318, 305], "Word", "Occurrence", "graphics/frequent_negative_words_negative_reviews.png"])
 thread12 = Thread(target=plot_graphic, args=["Most frequent positive words in negative reviews", ["fun", "good", "great", "fix", "friend", "support", "recommend", "well", "better"], [1137, 900, 648, 550, 505, 470, 446, 329, 312], "Word", "Occurrence", "graphics/frequent_positive_words_negative_reviews.png"])
-"""
+thread13 = Thread(target=plot_graphic, args=["Train times", classifiers_names, [10582.61030960083/60,0.030913352966308594/60,11155.967951536179/60,0.2752652168273926/60], "Algorithm (ngram_range)", "Minutes", "graphics/times.png"])
+
 #thread1.start()
 #thread1.join()
 #thread2.start()
@@ -91,7 +85,6 @@ thread12 = Thread(target=plot_graphic, args=["Most frequent positive words in ne
 #thread7.join()
 #thread8.start()
 #thread8.join()
-"""
 thread9.start()
 thread9.join()
 thread10.start()
@@ -100,6 +93,10 @@ thread11.start()
 thread11.join()
 thread12.start()
 thread12.join()
+thread13.start()
+thread13.join()
+
+set_reviews_to_wordcloud([reviews, reviews[:4999], reviews[5000:]])
 """
-# set_reviews_to_wordcloud([reviews, reviews[:4999], reviews[5000:]])
-#predict_texts(True, classifier4)
+
+predict_texts(True, classifier4)
